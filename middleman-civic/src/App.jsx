91b5869c409c2825cfa06be530326a351cbc2dfe
Mathew PC <mathewpunnen8281@gmail.com>
2026-02-27 16:08:28 +0530

    fix: resolve merge conflicts in App component routes

--- middleman-civic/src/App.jsx
+++ middleman-civic/src/App.jsx
@@ -2,13 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Requests from "./pages/Requests";
 import Heatmap from "./pages/Heatmap";
-<<<<<<< HEAD
 import AdminRequests from "./pages-admin/AdminRequests";
 import Login from "./pages/Login";
-
-=======
 import RequestDetails from "./pages/RequestDetails";
->>>>>>> 73f751f856bbd03c922cf749b77f2de59c481109
 function App() {
   return (
     <Router>
@@ -17,12 +13,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Requests />} />
           <Route path="/heatmap" element={<Heatmap />} />
-<<<<<<< HEAD
           <Route path="/admin" element={<AdminRequests />} />
           <Route path="/login" element={<Login />} />
-=======
           <Route path="/request/:id" element={<RequestDetails />} />
->>>>>>> 73f751f856bbd03c922cf749b77f2de59c481109
         </Routes>
       </div>
     </Router>
